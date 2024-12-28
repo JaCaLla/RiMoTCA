3202e651b2a168ebb1aee99322ff61f38cdaf122
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-28 09:12:49 +0100

    Initial Commit

---
+++ RiMoTCA/RiMoTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiMoTCAApp.swift
+//  RiMoTCA
+//
+//  Created by Javier Calatrava on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RiMoTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RiMoTCATests/RiMoTCATests.swift
@@ -0,0 +1,17 @@
+//
+//  RiMoTCATests.swift
+//  RiMoTCATests
+//
+//  Created by Javier Calatrava on 28/12/24.
+//
+
+import Testing
+@testable import RiMoTCA
+
+struct RiMoTCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
